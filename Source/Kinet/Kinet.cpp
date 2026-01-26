a3eaa4f875877f0ab6f3a065b2c1560d7d5fc92e
SangBeenKim <sb.kim03699@gmail.com>
2026-01-26 11:02:09 +0900

    chore : 프로젝트 초기화

---
+++ Source/Kinet/Kinet.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Kinet.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Kinet, "Kinet" );
