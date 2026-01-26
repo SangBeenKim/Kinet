4077d2533ef634dbaf251cd4b15781f6b62e639b
SangBeenKim <sb.kim03699@gmail.com>
2026-01-26 11:39:51 +0900

    chore : Title, Main 레벨 추가 및 GameMode 생성

---
+++ Source/Kinet/Game/KGameModeBase.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "KGameModeBase.generated.h"
+
+UCLASS()
+class KINET_API AKGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
