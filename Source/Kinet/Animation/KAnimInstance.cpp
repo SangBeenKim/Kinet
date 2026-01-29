ded53fa0f6405ec3190ff8ec0d4468e1060bf373
SangBeenKim <sb.kim03699@gmail.com>
2026-01-29 14:49:56 +0900

    feat : 캐릭터 이동, 점프 애니메이션 추가

---
+++ Source/Kinet/Animation/KAnimInstance.cpp
@@ -0,0 +1,27 @@
+﻿#include "Animation/KAnimInstance.h"
+#include "Character/KCharacterBase.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void UKAnimInstance::NativeInitializeAnimation()
+{
+	APawn* OwnerPawn = TryGetPawnOwner();
+	if (IsValid(OwnerPawn))
+	{
+		OwnerCharacter = Cast<AKCharacterBase>(OwnerPawn);
+		OwnerCharacterMovement = OwnerCharacter->GetCharacterMovement();
+	}
+}
+
+void UKAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	if (IsValid(OwnerCharacter) == true && IsValid(OwnerCharacterMovement) == true)
+	{
+		Velocity = OwnerCharacterMovement->Velocity;
+		GroundSpeed = Velocity.Size2D();
+
+		float GroundAcceleration = OwnerCharacterMovement->GetCurrentAcceleration().Size2D();
+		bool bIsAccelerationNearlyZero = FMath::IsNearlyZero(GroundAcceleration);
+		bShouldMove = (KINDA_SMALL_NUMBER < GroundSpeed) && (bIsAccelerationNearlyZero == false);
+		bIsFalling = OwnerCharacterMovement->IsFalling();
+	}
+}
