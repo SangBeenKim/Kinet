5e696b74302502ec01d7d49f4fa824310a8436c3
SangBeenKim <sb.kim03699@gmail.com>
2026-03-09 22:03:50 +0900

    wip : AnimInstance, AnimLayer 추가

---
+++ Source/Kinet/Animation/KCharacterActionAnimInstanceBase.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "KCharacterActionAnimInstanceBase.generated.h"
+
+UCLASS()
+class KINET_API UKCharacterActionAnimInstanceBase : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
